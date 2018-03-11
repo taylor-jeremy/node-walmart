f4ada95a8a69a3149b2e8b87eb7056b9eaf9bf66
Jeremy Taylor <tay16036@byui.edu>
2018-03-10 22:29:23 -0700

    modified index and form

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node index.js
+public: node index.js
---
+++ index.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var app = express();
+var url = require('url');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/stamp', function(request, response) {
+	handleStamp(request, response);
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+function handleStamp(request, response) {
+	var requestUrl = url.parse(request.url, true);
+
+	console.log("Query parameters: " + JSON.stringify(requestUrl.query));
+
+	// TODO: Here we should check to make sure we have all the correct parameters
+
+	var size = requestUrl.query.size;
+	var item_weight = Number(requestUrl.query.item_weight);
+
+	calculateRate(response, size, item_weight);
+}
+
+function calculateRate(response, size, weight) {
+	size = size.toLowerCase();
+
+	var cost = 0;
+
+	if (size == "Letters (Stamped)") {
+		if (weight < 1) {
+			cost = 0.50;
+		}
+	} else if (op == "subtract") {
+		result = left - right;		
+	} else if (op == "multiply") {
+		result = left * right;
+	} else if (op == "divide") {
+		result = left / right;
+	} else {
+		// It would be best here to redirect to an "unknown operation"
+		// error page or something similar.
+	}
+
+	// Set up a JSON object of the values we want to pass along to the EJS result page
+	var params = {operation: op, left: left, right: right, result: result};
+
+	// Render the response, using the EJS page "result.ejs" in the pages directory
+	// Makes sure to pass it the parameters we need.
+	response.render('pages/result', params);
+
+}
